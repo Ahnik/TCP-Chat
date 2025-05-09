ce03c233da1940bfe56915166f7350c140fd733c
Ahnik <sarkarahnik@gmail.com>
2025-05-09 07:38:01 +0000

    Defined constants, request and response types and response status as well as response and request structures

---
+++ include/common.h
@@ -0,0 +1,65 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdint.h>
+
+// Constants
+#define MAX_USERNAME_LEN 12
+#define MAX_MSG_SIZE 1024
+#define HEADER_SIZE 4
+
+// The types of requests supported
+typedef enum{
+    REQUEST_MSG = 0,    
+    REQUEST_JOIN, 
+    REQUEST_LEAVE,
+    REQUEST_NAME,
+    REQUEST_COUNT
+} RequestType;
+
+// The types of responses supported
+typedef enum{
+    RESPONSE_ACK = 0,
+    RESPONSE_ERR,
+    RESPONSE_INFO,
+    RESPONSE_COUNT
+} ResponseType;
+
+// The types of response statuses supported
+typedef enum{
+    // Status for ACK responses
+    STATUS_ACK_OK = 0,
+    STATUS_ACK_NAME_SET,
+    STATUS_ACK_JOINED,
+    STATUS_ACK_QUIT,
+
+    // Status for ERR responses
+    STATUS_ERR_INVALID_CMD,
+    STATUS_ERR_MALFORMED,
+    STATUS_ERR_NAME_TAKEN,
+    STATUS_ERR_NAME_MISSING,
+    STATUS_ERR_NOT_LOGGED_IN,
+
+    // Status for INFO responses
+    STATUS_INFO_USER_JOINED,
+    STATUS_INFO_USER_LEFT,
+    STATUS_INFO_MESSAGE,
+    STATUS_INFO_SERVER_SHUTDOWN,
+    STATUS_COUNT
+} ResponseStatus;
+
+// Request structure
+typedef struct{
+    RequestType type;
+    char username[MAX_USERNAME_LEN];
+    char message[MAX_MSG_SIZE]; 
+} Request;
+
+// Response structure
+typedef struct{
+    ResponseType type;
+    ResponseStatus status;
+    char message[MAX_MSG_SIZE + MAX_USERNAME_LEN + 1];
+} Response;
+
+#endif
