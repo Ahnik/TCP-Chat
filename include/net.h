5ec2bb82f6cfa6975241e8c26c343982549adda8
Ahnik <sarkarahnik@gmail.com>
2025-05-11 13:12:26 +0000

    Changed function names to reference payloads instead of requests

--- include/net.h
+++ include/net.h
@@ -4,14 +4,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// Function to read the first 4 bytes of an incoming request from the client socket and interpret it as the length of the request
-uint32_t read_request_len(int sockfd);
+// Function to read the first 4 bytes of an incoming message from the socket and interpret it as the length of the payload
+uint32_t read_payload_len(int sockfd);
 
 // Function to write the request length into a request buffer
-void write_request_len(uint8_t *buffer, uint32_t len);
+void write_payload_len(uint8_t *buffer, uint32_t len);
 
 // Functions to receive and send entire messages using TCP sockets to and from a buffer
-int recv_request(int sockfd, uint8_t *buffer, uint32_t length);
-int send_request(int sockfd, const uint8_t *buffer, uint32_t length);
+int recv_all(int sockfd, uint8_t *buffer, uint32_t length);
+int send_all(int sockfd, const uint8_t *buffer, uint32_t length);
 
 #endif
--- src/net.c
+++ src/net.c
@@ -3,7 +3,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-uint32_t read_request_len(int sockfd){
+uint32_t read_payload_len(int sockfd){
     uint32_t *len_buf = (uint32_t *)calloc(1, sizeof(*len_buf));
     if(!len_buf)
         exit_with_error("Memory allocation error");
@@ -20,12 +20,12 @@ uint32_t read_request_len(int sockfd){
     return length;
 }
 
-void write_request_len(uint8_t *buffer, uint32_t len){
+void write_payload_len(uint8_t *buffer, uint32_t len){
     uint32_t net_len = htonl(len);
     memcpy(buffer, &net_len, sizeof(uint32_t));
 }
 
-int recv_request(int sockfd, uint8_t *buffer, uint32_t length){
+int recv_all(int sockfd, uint8_t *buffer, uint32_t length){
     uint32_t bytesWritten = 0;
     while(bytesWritten < length){
         uint32_t bytesReceived = recv(sockfd, buffer + bytesWritten, length - bytesWritten, 0);
@@ -35,7 +35,7 @@ int recv_request(int sockfd, uint8_t *buffer, uint32_t length){
     return 0;
 }
 
-int send_request(int sockfd, const uint8_t *buffer, uint32_t length){
+int send_all(int sockfd, const uint8_t *buffer, uint32_t length){
     uint32_t total_bytes = 0;
     while(total_bytes < length){
         uint32_t bytesSent = send(sockfd, buffer + total_bytes, length - total_bytes, 0);
