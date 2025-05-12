e2014975087505a326e5257ccf457bfe8cf9159d
Ahnik <sarkarahnik@gmail.com>
2025-05-12 10:26:49 +0000

    Moved the struct StatusEntry and the status_table to protocol.c since it is part of the implementation

--- src/protocol.c
+++ src/protocol.c
@@ -1,6 +1,29 @@
 #include "protocol.h"
 #include <string.h>
 
+// Creating a lookup table of status codes and their corresponding strings 
+struct StatusEntry{
+    const char *str;
+    ResponseStatus status;
+};
+
+static const struct StatusEntry status_table[] = {
+    {"OK"             , STATUS_ACK_OK},
+    {"NAME_SET"       , STATUS_ACK_NAME_SET},
+    {"JOINED"         , STATUS_ACK_JOINED},
+    {"QUIT"           , STATUS_ACK_QUIT},
+    {"INVALID_CMD"    , STATUS_ERR_INVALID_CMD},
+    {"MALFORMED"      , STATUS_ERR_MALFORMED},
+    {"NAME_MISSING"   , STATUS_ERR_NAME_MISSING},
+    {"NAME_TAKEN"     , STATUS_ERR_NAME_TAKEN},
+    {"NOT_LOGGED_IN"  , STATUS_ERR_NOT_LOGGED_IN},
+    {"USER_JOINED"    , STATUS_INFO_USER_JOINED},
+    {"USER_LEFT"      , STATUS_INFO_USER_LEFT},
+    {"MESSAGE"        , STATUS_INFO_MESSAGE},
+    {"SERVER_SHUTDOWN", STATUS_INFO_SERVER_SHUTDOWN}
+};
+
+
 RequestType parse_request_type(const char *type_str){
     if(strcmp(type_str, "MSG") == 0)
         return REQUEST_MSG;
