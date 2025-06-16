02f6bea835bde71a510920868df063ed042eea6c
Ahnik <sarkarahnik@gmail.com>
2025-06-16 08:03:25 +0000

    Finished test_handle_leave_request(). Test passed

--- tests/test_request_handler.c
+++ tests/test_request_handler.c
@@ -175,6 +175,9 @@ void test_handle_join_request(){
         TEST_ASSERT_EQUAL(RESPONSE_ACK, response->type);
         TEST_ASSERT_EQUAL(STATUS_ACK_JOINED, response->status);
 
+        TEST_ASSERT_EQUAL_INT(i+1, count_clients());
+        TEST_ASSERT_NOT_NULL(find_client_by_socket(socket_pairs[i][0]));
+
         for(int j=0; j<=i; j++){
             bzero(response_buffer, MAX_PAYLOAD_SIZE);
             bzero(response, sizeof(*response));
@@ -183,7 +186,7 @@ void test_handle_join_request(){
             snprintf(payload, MAX_PAYLOAD_SIZE, "INFO|USER_JOINED|--%s has joined--", name);
 
             length = read_payload_len(socket_pairs[j][1]);
-            //TEST_ASSERT_EQUAL_UINT32((uint32_t)(strlen(payload)), length);
+            TEST_ASSERT_EQUAL_UINT32((uint32_t)(strlen(payload)), length);
             TEST_ASSERT_EQUAL_INT(ERR_OK, recv_all(socket_pairs[j][1], (uint8_t *)response_buffer, length));
             response_buffer[length] = 0;
             TEST_ASSERT_EQUAL_STRING(payload, response_buffer);
@@ -199,10 +202,69 @@ void test_handle_join_request(){
     }
 }
 
+void test_handle_leave_request(){
+    int socket_pairs[MAX_CLIENTS][2];   
+    char name[2] = "a";
+    for(int i=0; i<MAX_CLIENTS; i++){
+        if(socketpair(AF_UNIX, SOCK_STREAM, 0, socket_pairs[i]) != 0) TEST_IGNORE();
+        if(add_client(socket_pairs[i][0], name) != ERR_OK) TEST_IGNORE();   // 0 is for sending, 1 is for receiving
+        name[0]++;
+    }
+
+    name[0] = 'a';
+    for(int i=0; i<MAX_CLIENTS; i++){
+        Request *request = (Request *)malloc(sizeof(*request));
+        request->type = REQUEST_LEAVE;
+        snprintf(request->username, MAX_MESSAGE_SIZE, "%s", name);
+
+        handle_leave_request(socket_pairs[i][0], request);
+
+        char response_buffer[MAX_PAYLOAD_SIZE];
+        bzero(response_buffer, MAX_PAYLOAD_SIZE);
+
+        uint32_t length = read_payload_len(socket_pairs[i][1]);
+        TEST_ASSERT_EQUAL_UINT32((uint32_t)(strlen("ACK|QUIT")), length);
+        TEST_ASSERT_EQUAL_INT(ERR_OK, recv_all(socket_pairs[i][1], (uint8_t *)response_buffer, length));
+        response_buffer[length] = 0;
+        TEST_ASSERT_EQUAL_STRING("ACK|QUIT", response_buffer);
+
+        Response *response = (Response *)malloc(sizeof(*response));
+        TEST_ASSERT_EQUAL_INT(ERR_OK, decode_response(response, response_buffer));
+        TEST_ASSERT_EQUAL(RESPONSE_ACK, response->type);
+        TEST_ASSERT_EQUAL(STATUS_ACK_QUIT, response->status);
+
+        TEST_ASSERT_EQUAL_INT(MAX_CLIENTS-i-1, count_clients());
+        TEST_ASSERT_NULL(find_client_by_socket(socket_pairs[i][0]));
+
+        for(int j=i+1; j<MAX_CLIENTS; j++){
+            bzero(response_buffer, MAX_PAYLOAD_SIZE);
+            bzero(response, sizeof(*response));
+
+            char payload[MAX_PAYLOAD_SIZE];
+            snprintf(payload, MAX_PAYLOAD_SIZE, "INFO|USER_LEFT|--%s has left--", name);
+
+            length = read_payload_len(socket_pairs[j][1]);
+            TEST_ASSERT_EQUAL_UINT32((uint32_t)(strlen(payload)), length);
+            TEST_ASSERT_EQUAL_INT(ERR_OK, recv_all(socket_pairs[j][1], (uint8_t *)response_buffer, length));
+            response_buffer[length] = 0;
+            TEST_ASSERT_EQUAL_STRING(payload, response_buffer);
+
+            TEST_ASSERT_EQUAL_INT(ERR_OK, decode_response(response, response_buffer));
+            TEST_ASSERT_EQUAL(RESPONSE_INFO, response->type);
+            TEST_ASSERT_EQUAL(STATUS_INFO_USER_LEFT, response->status);
+        }
+
+        free(response);
+        free(request);
+        name[0]++;
+    }
+}
+
 int main(){
     UNITY_BEGIN();
     RUN_TEST(test_handle_name_request);
     RUN_TEST(test_handle_msg_request);
     RUN_TEST(test_handle_join_request);
+    RUN_TEST(test_handle_leave_request);
     return UNITY_END();
 }
